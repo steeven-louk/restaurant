4a5cf33cdf303bad241517cc6bf833051457cfe0
steeven-louk <steevenlouk.sl@gmail.com>
2023-11-02 18:26:23 +0100

    mise en place du slider et footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
+import Featured from "@/components/Featured";
+import Offer from "@/components/Offer";
+import Slider from "@/components/Slider";
 
 export default function Home() {
   return (
     <main>
+      <Slider/>
       hello world
+      <Featured/>
+      <Offer/>
     </main>
   )
 }
---
+++ src/components/Slider.tsx
@@ -0,0 +1,52 @@
+"use client"
+import Image from 'next/image'
+import React, { useState, useEffect } from 'react'
+
+const data = [
+  {
+    id: 1,
+    title: "always fresh & always crispy & always hot",
+    image: "/slide1.png"
+  },
+  {
+    id: 2,
+    title: "we deliver your order wherever you are in NY",
+    image: "/slide2.png"
+  },
+  {
+    id: 3,
+    title: "the best pizza to share with your family",
+    image: "/slide3.jpg"
+  },
+];
+
+
+const Slider = () => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  // useEffect(() => {
+  //   const interval = setInterval(
+  //     ()=> setCurrentSlide((prev)=> (prev === data.length -1 ? 0 : prev +1)),2000);
+  //   return ()=> clearInterval(interval);
+  // }, []);
+
+  return (
+    <div className='flex flex-col h-[calc(100vh - 6rem)] md:h-[calc(100vh-9rem)] lg:flex-row bg-fuchsia-50'>
+      {/* TEXT CONTAINER */}
+      <div className="flex-1 h-1/2 flex items-center justify-center flex-col gap-8 text-red-500 font-bold lg:h-full lg:w-1/2">
+        <h1 className='text-5xl text-center uppercase p-4 md:p-10 md:text-6xl xl:text-7xl'>
+          {data[currentSlide]?.title}
+        </h1>
+        <button className='bg-red-500 text-white py-4 px-8'>
+          Order now
+        </button>
+      </div>
+      {/* IMAGE CONTAINER */}
+      <div className="w-full flex-1 relative h-1/2 lg:h-full lg:w-1/2">
+        <Image src={data[currentSlide]?.image} alt={data[currentSlide]?.title} fill className=' object-cover' />
+      </div>
+    </div>
+  )
+}
+
+export default Slider
