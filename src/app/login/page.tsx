b5aad73a96276ef07941b034a563881a8f10c914
steeven-louk <steevenlouk.sl@gmail.com>
2023-11-07 16:05:33 +0100

    mise en place de la page orders

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -17,12 +17,12 @@ const LoginPage = () => {
         <h1 className='font-bold text-xl xl:text-3xl'>Welcome</h1>
         <p>Log into your account ot create a new one using social buttons</p>
         <button className="flex gap-4 p-4 ring-1 ring-orange-100 rounded-md">
-          <Image src={"/google.png"} alt="" width={20} height={20} className='object-contain' />
+          <Image src={"/google.png"} alt="google image" width={20} height={20} className='object-contain' />
           <span>Sign in with Google</span>
         </button>
 
         <button className="flex gap-4 p-4 ring-1 ring-orange-100 rounded-md">
-          <Image src={"/facebook.png"} alt="" width={20} height={20} className='object-contain' />
+          <Image src={"/facebook.png"} alt="facebook image" width={20} height={20} className='object-contain' />
           <span>Sign in with Facebook</span>
         </button>
         <p className='text-sm'>Have a problem? <Link className='underline' href={"/"}>Contact us</Link></p>
